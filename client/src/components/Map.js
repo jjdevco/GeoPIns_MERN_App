1474d80a0a69b26f58eaac550e27c4157e08128f
Jose Jimenez <58991130+jjdevco@users.noreply.github.com>
2020-08-14 22:00:55 -0500

    Placing a Pin for the user current position

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import MapGL, { NavigationControl } from "@urbica/react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-// import ReactMapGl, { NavigationControl } from "react-map-gl";
+import MapGL, { NavigationControl, Marker } from "@urbica/react-map-gl";
+
+import PinIcon from "./PinIcon";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
@@ -16,8 +16,29 @@ const Map = ({ classes }) => {
     zoom: 12,
   });
 
+  const [userPosition, setUserPosition] = useState(null);
+
+  const onDragEnd = (lngLat) => {
+    setUserPosition({ longitude: lngLat.lng, latitude: lngLat.lat });
+  };
+
+  const getUserPosition = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setViewport((viewport) => ({ ...viewport, latitude, longitude }));
+        setUserPosition({ latitude, longitude });
+      });
+    }
+  };
+
+  useEffect(() => {
+    getUserPosition();
+  }, []);
+
   return (
     <div className={classes.root}>
+      <h1>{userPosition}</h1>
       <MapGL
         style={{ width: "100vw", height: "calc(100vh - 64px)" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
@@ -30,6 +51,20 @@ const Map = ({ classes }) => {
         <div className={classes.navigationControl}>
           <NavigationControl showCompass showZoom position="top-right" />
         </div>
+
+        {/* Pin for User's current Position */}
+        {userPosition && (
+          <Marker
+            latitude={viewport.latitude}
+            longitude={viewport.longitude}
+            offsetLeft={19}
+            offsetTop={-37}
+            onDragEnd={onDragEnd}
+            draggable
+          >
+            <PinIcon size={40} color="red" />
+          </Marker>
+        )}
       </MapGL>
     </div>
   );
